b1ed8c4c29c1ee6d7441d3169a7baf2092e63137
alanyur <alanibagon@gmail.com>
2024-12-06 16:49:00 -0300

    implementacion en rama doctor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Container1 from './pages/container1';
 import ScheduledAppointments from './components/ScheduledAppointments';  // Importa la vista de citas agendadas
-import Funcionario from "./pages/funcionario";
 import Doctor from "./pages/doctor";
 import LoginFunEsp from './pages/LoginFunEspe';
 import Browser from './pages/Browser';
@@ -11,7 +10,6 @@ function App() {
     return (
       <Routes>
         <Route path="/" element={<Container1 />} />
-        <Route path="/funcionario" element={<Funcionario />} />
         <Route path="/doctor" element={<Doctor />} />
         <Route path="/patients/:rut/appointments" element={<ScheduledAppointments />} />
         <Route path='/LoginFunEsp' element={<LoginFunEsp />}/>
