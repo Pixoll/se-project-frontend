a26e6fd64ff56db69801de9043d9c17bce29d140
Pixoll <pixoll.gamer@gmail.com>
2024-12-06 15:15:56 -0300

    Display appointments from API

--- src/pages/PatientHome.tsx
+++ src/pages/PatientHome.tsx
@@ -1,54 +1,80 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Calendar, { TileClassNameFunc } from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "../styles/PatientHome.css";
+import { useAuth } from "../hooks/useAuth";
+import useFetch from "../hooks/useFetch";
 
 type Appointment = {
-    id: number;
-    date: Date;
-    specialist: string;
-    patient: {
-        name: string;
-        id: string;
-        details: string;
-    },
+    id: string;
+    medicRut: string;
+    date: string;
+    start: string;
+    end: string;
+    description: string;
     confirmed: boolean;
-}
+};
 
+type Medic = {
+    rut: string;
+    fullName: string;
+    email: string;
+    phone: number;
+    specialty: string;
+};
+
+type ParsedAppointment = {
+    id: string;
+    medic: Medic;
+    date: string;
+    start: string;
+    end: string;
+    description: string;
+    confirmed: boolean;
+};
 
 export default function PatientHome() {
+    const { state } = useAuth();
     const [date, setDate] = useState(new Date());
-    const [appointments, setAppointments] = useState<Appointment[]>([]);
+    const appointmentsFetchResult = useFetch<Appointment[]>(`/patients/${state.rut}/appointments`);
+    const medicsFetchResult = useFetch<Medic[]>("/medics");
 
-    useEffect(() => {
-        const fetchAppointments = async () => {
-            try {
-                const response = await fetch(""); // colocar url
-                const data = await response.json();
-                setAppointments(data);
-            } catch (error) {
-                console.error("Error al cargar citas:", error);
-            }
-        };
+    if (appointmentsFetchResult.status === "loading" || medicsFetchResult.status === "loading") {
+        return <div>Loading...</div>;
+    }
 
-        fetchAppointments();
-    }, []);
+    if (appointmentsFetchResult.status === "failed" || medicsFetchResult.status === "failed") {
+        // @ts-ignore
+        return <div>Error: {appointmentsFetchResult.error ?? medicsFetchResult.error}</div>;
+    }
 
-    const handleCancelAppointment = (id: number) => {
-        setAppointments(appointments.filter(appointment => appointment.id !== id));
+    const medics = new Map(medicsFetchResult.data.map(m => [m.rut, m]));
+
+    const appointments: ParsedAppointment[] = appointmentsFetchResult.data.map(a => ({
+        id: a.id,
+        medic: medics.get(a.medicRut)!,
+        date: a.date,
+        start: a.start,
+        end: a.end,
+        description: a.description,
+        confirmed: a.confirmed,
+    }));
+
+    const handleCancelAppointment = (id: string) => {
+        // setAppointments(appointments.filter(appointment => appointment.id !== id));
         alert("Cita cancelada");
     };
 
     const tileClassName: TileClassNameFunc = ({ date, view }) => {
         if (view === "month") {
             const hasConfirmedAppointment = appointments.some(appointment =>
-                appointment.date.toDateString() === date.toDateString() && appointment.confirmed
+                appointment.date === toDateString(date) && appointment.confirmed
             );
             if (hasConfirmedAppointment) {
                 return "confirmed-day";
             }
             const hasAppointment = appointments.some(appointment =>
-                appointment.date.toDateString() === date.toDateString()
+                appointment.date === toDateString(date)
             );
             return hasAppointment ? "highlighted-day" : null;
         }
@@ -71,10 +97,10 @@ export default function PatientHome() {
         const startDateTime = new Date(date);
         startDateTime.setHours(parseInt(startHour, 10), parseInt(startMinute, 10), 0, 0);
 
-        const appointment = appointments.find(app => app.date.getTime() === startDateTime.getTime());
+        const appointment = appointments.find(app => app.date === toDateString(startDateTime));
         return appointment ? (
             <div className="appointment-details">
-                <p>Con: {appointment.specialist}</p>
+                <p>Con: {appointment.medic.fullName}</p>
                 <button className="nav-button" onClick={() => handleCancelAppointment(appointment.id)}>
                     Cancelar Cita
                 </button>
@@ -86,18 +112,18 @@ export default function PatientHome() {
 
     return (
         <div className="scheduled-appointments">
-            <h1 className="welcome">Tus Citas Agendadas</h1>
+            <h1 className="welcome">Tus citas agendadas</h1>
             <div className="calendar-and-schedule">
                 <div className={"appointment-list-container"}>
-                    <h2 className={"sub-welcome"}>Lista de Citas</h2>
+                    <h2 className={"sub-welcome"}>Lista de citas</h2>
                     <ul>
                         {appointments.map(appointment => (
                             <li key={appointment.id} className={"appointment"}>
-                                <p>Fecha: {appointment.date.toLocaleDateString()}</p>
-                                <p>Especialista: {appointment.specialist}</p>
-                                <p>Paciente: {appointment.patient.name}</p>
-                                <button className={"nav-button"} onClick={() => handleCancelAppointment(appointment.id)}>
-                                    Cancelar Cita
+                                <p>Fecha: {appointment.date}</p>
+                                <p>Especialista: {appointment.medic.fullName}</p>
+                                <button className={"nav-button"}
+                                        onClick={() => handleCancelAppointment(appointment.id)}>
+                                    Cancelar cita
                                 </button>
                             </li>
                         ))}
@@ -128,3 +154,7 @@ export default function PatientHome() {
         </div>
     );
 };
+
+function toDateString(date: Date): string {
+    return date.toLocaleDateString("es-CL").split("-").reverse().join("-");
+}
